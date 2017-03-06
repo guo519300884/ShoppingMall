56c88f6f5b187fe05ae8511b53627c66deec2fb5
guo519300884 <519300884@qq.com>
2017-03-06 16:36:19 +0800

    29.实现筛选菜单的显示 & 实现其点击事件

--- app/src/main/java/shoppingmall/home/activity/GoodsListActivity.java
+++ app/src/main/java/shoppingmall/home/activity/GoodsListActivity.java
@@ -7,10 +7,17 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -56,6 +63,128 @@ public class GoodsListActivity extends AppCompatActivity {
     RecyclerView recyclerview;
     @InjectView(R.id.dl_left)
     DrawerLayout dlLeft;
+    @InjectView(R.id.ib_drawer_layout_back)
+    ImageButton ibDrawerLayoutBack;
+    @InjectView(R.id.tv_ib_drawer_layout_title)
+    TextView tvIbDrawerLayoutTitle;
+    @InjectView(R.id.ib_drawer_layout_confirm)
+    TextView ibDrawerLayoutConfirm;
+    @InjectView(R.id.rb_select_hot)
+    RadioButton rbSelectHot;
+    @InjectView(R.id.rb_select_new)
+    RadioButton rbSelectNew;
+    @InjectView(R.id.rg_select)
+    RadioGroup rgSelect;
+    @InjectView(R.id.tv_drawer_price)
+    TextView tvDrawerPrice;
+    @InjectView(R.id.tv_drawer_recommend)
+    TextView tvDrawerRecommend;
+    @InjectView(R.id.rl_select_recommend_theme)
+    RelativeLayout rlSelectRecommendTheme;
+    @InjectView(R.id.tv_drawer_type)
+    TextView tvDrawerType;
+    @InjectView(R.id.rl_select_type)
+    RelativeLayout rlSelectType;
+    @InjectView(R.id.btn_select_all)
+    Button btnSelectAll;
+    @InjectView(R.id.ll_select_root)
+    LinearLayout llSelectRoot;
+    @InjectView(R.id.btn_drawer_layout_cancel)
+    Button btnDrawerLayoutCancel;
+    @InjectView(R.id.btn_drawer_layout_confirm)
+    Button btnDrawerLayoutConfirm;
+    @InjectView(R.id.iv_price_no_limit)
+    ImageView ivPriceNoLimit;
+    @InjectView(R.id.rl_price_nolimit)
+    RelativeLayout rlPriceNolimit;
+    @InjectView(R.id.iv_price_0_15)
+    ImageView ivPrice015;
+    @InjectView(R.id.rl_price_0_15)
+    RelativeLayout rlPrice015;
+    @InjectView(R.id.iv_price_15_30)
+    ImageView ivPrice1530;
+    @InjectView(R.id.rl_price_15_30)
+    RelativeLayout rlPrice1530;
+    @InjectView(R.id.iv_price_30_50)
+    ImageView ivPrice3050;
+    @InjectView(R.id.rl_price_30_50)
+    RelativeLayout rlPrice3050;
+    @InjectView(R.id.iv_price_50_70)
+    ImageView ivPrice5070;
+    @InjectView(R.id.rl_price_50_70)
+    RelativeLayout rlPrice5070;
+    @InjectView(R.id.iv_price_70_100)
+    ImageView ivPrice70100;
+    @InjectView(R.id.rl_price_70_100)
+    RelativeLayout rlPrice70100;
+    @InjectView(R.id.iv_price_100)
+    ImageView ivPrice100;
+    @InjectView(R.id.rl_price_100)
+    RelativeLayout rlPrice100;
+    @InjectView(R.id.et_price_start)
+    EditText etPriceStart;
+    @InjectView(R.id.v_price_line)
+    View vPriceLine;
+    @InjectView(R.id.et_price_end)
+    EditText etPriceEnd;
+    @InjectView(R.id.rl_select_price)
+    RelativeLayout rlSelectPrice;
+    @InjectView(R.id.ll_price_root)
+    LinearLayout llPriceRoot;
+    @InjectView(R.id.btn_drawer_theme_cancel)
+    Button btnDrawerThemeCancel;
+    @InjectView(R.id.btn_drawer_theme_confirm)
+    Button btnDrawerThemeConfirm;
+    @InjectView(R.id.iv_theme_all)
+    ImageView ivThemeAll;
+    @InjectView(R.id.rl_theme_all)
+    RelativeLayout rlThemeAll;
+    @InjectView(R.id.iv_theme_note)
+    ImageView ivThemeNote;
+    @InjectView(R.id.rl_theme_note)
+    RelativeLayout rlThemeNote;
+    @InjectView(R.id.iv_theme_funko)
+    ImageView ivThemeFunko;
+    @InjectView(R.id.rl_theme_funko)
+    RelativeLayout rlThemeFunko;
+    @InjectView(R.id.iv_theme_gsc)
+    ImageView ivThemeGsc;
+    @InjectView(R.id.rl_theme_gsc)
+    RelativeLayout rlThemeGsc;
+    @InjectView(R.id.iv_theme_origin)
+    ImageView ivThemeOrigin;
+    @InjectView(R.id.rl_theme_origin)
+    RelativeLayout rlThemeOrigin;
+    @InjectView(R.id.iv_theme_sword)
+    ImageView ivThemeSword;
+    @InjectView(R.id.rl_theme_sword)
+    RelativeLayout rlThemeSword;
+    @InjectView(R.id.iv_theme_food)
+    ImageView ivThemeFood;
+    @InjectView(R.id.rl_theme_food)
+    RelativeLayout rlThemeFood;
+    @InjectView(R.id.iv_theme_moon)
+    ImageView ivThemeMoon;
+    @InjectView(R.id.rl_theme_moon)
+    RelativeLayout rlThemeMoon;
+    @InjectView(R.id.iv_theme_quanzhi)
+    ImageView ivThemeQuanzhi;
+    @InjectView(R.id.rl_theme_quanzhi)
+    RelativeLayout rlThemeQuanzhi;
+    @InjectView(R.id.iv_theme_gress)
+    ImageView ivThemeGress;
+    @InjectView(R.id.rl_theme_gress)
+    RelativeLayout rlThemeGress;
+    @InjectView(R.id.ll_theme_root)
+    LinearLayout llThemeRoot;
+    @InjectView(R.id.btn_drawer_type_cancel)
+    Button btnDrawerTypeCancel;
+    @InjectView(R.id.btn_drawer_type_confirm)
+    Button btnDrawerTypeConfirm;
+    @InjectView(R.id.expandableListView)
+    ExpandableListView expandableListView;
+    @InjectView(R.id.ll_type_root)
+    LinearLayout llTypeRoot;
 
     private int position;
     /**
@@ -82,7 +211,7 @@ public class GoodsListActivity extends AppCompatActivity {
     private void getData() {
         position = getIntent().getIntExtra("position", 0);
         getDataFromNet(urls[position]);
-        //默认综合排序
+
         initView();
     }
 
@@ -92,6 +221,7 @@ public class GoodsListActivity extends AppCompatActivity {
         //设置价格 & 筛选为默认
         tvGoodsListPrice.setTextColor(Color.parseColor("#333538"));
         tvGoodsListSelect.setTextColor(Color.parseColor("#333538"));
+        showLinearLayout(llSelectRoot);
     }
 
     private void getDataFromNet(String url) {
@@ -143,7 +273,7 @@ public class GoodsListActivity extends AppCompatActivity {
         }
     }
 
-    @OnClick({R.id.ib_goods_list_back, R.id.tv_goods_list_search, R.id.ib_goods_list_home, R.id.tv_goods_list_sort, R.id.tv_goods_list_price, R.id.tv_goods_list_select})
+    @OnClick({R.id.ib_goods_list_back, R.id.btn_drawer_theme_confirm, R.id.tv_goods_list_search, R.id.ib_goods_list_home, R.id.tv_goods_list_sort, R.id.tv_goods_list_price, R.id.tv_goods_list_select, R.id.ib_drawer_layout_back, R.id.ib_drawer_layout_confirm, R.id.rl_select_price, R.id.rl_select_recommend_theme, R.id.rl_select_type, R.id.btn_drawer_layout_cancel, R.id.btn_drawer_layout_confirm, R.id.btn_drawer_theme_cancel, R.id.btn_drawer_type_cancel, R.id.btn_drawer_type_confirm})
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.ib_goods_list_back:
@@ -184,7 +314,58 @@ public class GoodsListActivity extends AppCompatActivity {
                 //设置价格 & 筛选为默认
                 tvGoodsListSort.setTextColor(Color.parseColor("#333538"));
                 tvGoodsListPrice.setTextColor(Color.parseColor("#333538"));
+
+                //打开DrawLayout
+                dlLeft.openDrawer(Gravity.RIGHT);
+                break;
+            case R.id.ib_drawer_layout_back:
+                dlLeft.closeDrawers();
+                break;
+            case R.id.ib_drawer_layout_confirm:
+                Toast.makeText(this, "筛选 确定", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.rl_select_price:
+//                showPriceLayout();
+                showLinearLayout(llPriceRoot);
+                break;
+            case R.id.rl_select_recommend_theme:
+//                showThemeLayout();
+                showLinearLayout(llThemeRoot);
+                break;
+            case R.id.rl_select_type:
+//                showTypeLayout();
+                showLinearLayout(llTypeRoot);
+                break;
+            case R.id.btn_drawer_layout_cancel:
+                showLinearLayout(llSelectRoot);
+                break;
+            case R.id.btn_drawer_layout_confirm:
+                Toast.makeText(this, "价格 确定", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_drawer_theme_cancel:
+                showLinearLayout(llSelectRoot);
+                break;
+            case R.id.btn_drawer_theme_confirm:
+                Toast.makeText(this, "主题 确定", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btn_drawer_type_cancel:
+                showLinearLayout(llSelectRoot);
+                break;
+            case R.id.btn_drawer_type_confirm:
+                Toast.makeText(this, "嘎嘎", Toast.LENGTH_SHORT).show();
                 break;
         }
     }
+
+
+    //显示l布局
+    private void showLinearLayout(LinearLayout l) {
+        llPriceRoot.setVisibility(View.GONE);
+        llThemeRoot.setVisibility(View.GONE);
+        llTypeRoot.setVisibility(View.GONE);
+        llSelectRoot.setVisibility(View.GONE);
+        l.setVisibility(View.VISIBLE);
+    }
+
+
 }
